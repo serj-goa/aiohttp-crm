eaaf26e9ca84a8fa8221e0b55786954b46ed6b60
Serhii Borodin <69416078+serj-goa@users.noreply.github.com>
2023-02-17 00:01:30 +0300

    Update based class aiohttp.web
    Update base class aiohttp.web - Application, Request, View

--- app/web/app.py
+++ app/web/app.py
@@ -1,6 +1,25 @@
-from aiohttp.web import Application, run_app as aiohttp_run_app
+from aiohttp.web import run_app as aiohttp_run_app, \
+                        Application as AiohttpApplication, \
+                        Request as AiohttpRequest, \
+                        View as AiohttpView
 
 from app.web.routes import setup_routes
+
+
+class Application(AiohttpApplication):
+    database: dict = {}
+
+
+class Request(AiohttpRequest):
+    @property
+    def app(self) -> Application:
+        return super().app()
+
+
+class View(AiohttpView):
+    @property
+    def request(self) -> Request:
+        return super().request
 
 
 app = Application()
