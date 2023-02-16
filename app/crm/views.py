d7041c0dc98f7c9cd61db9cf2c1ae7b75cab4ff0
Serhii Borodin <69416078+serj-goa@users.noreply.github.com>
2023-02-17 00:05:15 +0300

    Add view /add_user

--- app/crm/views.py
+++ app/crm/views.py
@@ -1,5 +1,13 @@
+import uuid
+
 from aiohttp.web_response import json_response
 
+from app.crm.models import User
+from app.web.app import View
 
-def index(request):
-    return json_response(data={'data': 'hello'})
+
+class AddUserView(View):
+    async def post(self):
+        data = await self.request.json()
+        user = User(email=data['email'], _id=uuid.uuid4())
+        return json_response(data={'status': 'ok'})
